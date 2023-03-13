8644ef2242ca3f8055582d7896578efdf0eb7035
alexKos58 <alex_kostyushin@mail.ru>
2023-03-13 18:45:19 +0300

    Change ColorSchema.java

--- src/ru/netology/graphics/image/ColorSchema.java
+++ src/ru/netology/graphics/image/ColorSchema.java
@@ -1,6 +1,11 @@
 package ru.netology.graphics.image;
 
 public class ColorSchema implements TextColorSchema{
+    /**
+     *
+     * @param color - интенсивность цвета пикселя
+     * @return символ подходящий для замены
+     */
     @Override
     public char convert(int color) {
         char[] arr = {'#', '$', '@', '%', '*', '+', '-', '\''};
